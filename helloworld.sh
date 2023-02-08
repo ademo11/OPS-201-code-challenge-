3fa231291d5322ddac861902f00b877087a85452
ademo11 <ademo11@devbox1>
2023-02-08 03:39:28 +0000

    this is my commit message

---
+++ helloworld.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script: OPS 201 Class 02 Ops Challenge Solution
+# Author: Ademola
+# Date of latest revision: 7 Feb 2023
+# Purpose: Is to create a sript that will print I am working perfectly to the screen
+
+
+#Main
+
+
+pwd
+whoami
+
+#end
