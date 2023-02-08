da5d82e67135839a08ac2e4f93068b0a382efe17
ademo11 <ademo11@devbox1>
2023-02-08 20:47:58 +0000

    this is my commit message

--- helloworld.sh
+++ helloworld.sh
@@ -1,12 +1,12 @@
-#main
+git#main
 #!/bin/bash
 
 # Script: OPS 201 Class 02 Ops Challenge Solution
 # Author: Ademola
 # Date of latest revision: 7 Feb 2023
 # Purpose: Is to create a sript that will print I am working perfectly to the screen
- + print("I am working perfect")
- + #!usr/bin/env pyton
+ + echo("I am working perfect")
+ 
 
 pwd
 whoami
