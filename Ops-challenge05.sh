95add1b8708c3e01ff65c6dce024a92a5ca2d744
ademo11 <ademo11@devbox1>
2023-02-11 05:32:26 +0000

    this is my commit time 11:33pct

--- Ops-challenge05.sh
+++ Ops-challenge05.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-# Write a script that:
+# Write a script that displays running processes
+echo "displays running processes"
+
+echo "PID TTY TIME CMD"
+echo  $$
+while true; do 
+read -p "16796: " pid 
+# kill the process with the given PID
+echo "kill the process with PID $PID..."
+kill $PID
+# confirm that the process has been killed
+echo "process with PID $16796 has been killed"
+
+end
+
+# asks the user for a PID, then kills the process with that PID
+kill -9 $PID
+
+echo "kill has been completed"
