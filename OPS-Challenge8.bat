b1c80c8adb8de6f202e716494d78940dba95c702
Ademola Olatunbosun <ademomo11@gmail.com>
2023-02-20 17:26:32 +0000

    this is my commit message

--- OPS-Challenge8.bat
+++ OPS-Challenge8.bat
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Script: OPS 201 Class 02 Ops Challenge Solution
+# Author: Ademola
+# Date of latest revision: 20 Feb 20 23
+# Purpose:
 
 robocopy "C:\Users\j.thompson" "\Desktop\My Project Files"
 
 # end 
 # done
+
+# it say Robocopy from the C drive and find this user J.thompson, copy it from my deskstop and find a file name My project file and robocopy at this set time everyday.
