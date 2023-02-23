74311cbf0f73c5a4dc4206e63aa78e60a0852dc7
Ademola Olatunbosun <ademomo11@gmail.com>
2023-02-23 01:56:40 +0000

    this is my message

--- OPS-Challenge 13.sh
+++ OPS-Challenge 13.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Script: OPS 201 Class 02 Ops Challenge Solution
+# Author: Ademola
+# Date of latest revision: 17 Feb 20 23
+# Purpose: To
+
 
 # define the function to take user input string (domain name)
 function get_domain_name {
